df72ff7d38b367ac5ebc8dd342f20586884a4597
tdubsxc <trdubs90@gmail.com>
2023-11-02 18:05:40 -0400

    Added scroll animations.

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,3 @@
 import "./modules/faq.js";
 import "./modules/page-load.js";
+import "./modules/scroll-animations.js";
---
+++ assets/modules/scroll-animations.js
@@ -0,0 +1,8 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  offset: 100,
+  duration: 500,
+  once: true,
+});
