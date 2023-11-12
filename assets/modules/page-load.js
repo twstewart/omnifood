502fbe405f46876b7c6f200cb6f68a064522c11f
tdubsxc <trdubs90@gmail.com>
2023-11-12 18:16:02 -0500

    Final commit.

--- assets/modules/page-load.js
+++ assets/modules/page-load.js
@@ -1,6 +1,9 @@
 const viewport = document.querySelector("meta[name=viewport]");
+const year = document.querySelector("#year");
 
 window.addEventListener("DOMContentLoaded", () => {
   document.body.classList.remove("preload");
   viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
+
+  year.textContent = new Date().getFullYear();
 });
